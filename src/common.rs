67e32b7370c13ffd2f315ebc4fdbe9476073a537
Igor Petruk <ipetruk@google.com>
2019-11-02 22:37:00 +0000

    Added common.rs #5

---
+++ src/common.rs
@@ -0,0 +1,78 @@
+use failure::{format_err, Error, ResultExt};
+use log::debug;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+
+pub fn build_cache_path(script_path: &Path) -> Result<PathBuf, Error> {
+    let script_path = script_path
+        .canonicalize()
+        .context("Cannot build full path from given script path")?;
+    let script_path_rel = script_path
+        .strip_prefix("/")
+        .context(format!("Could not strip '/' prefix from {:?}", script_path))?;
+
+    let mut user_cache =
+        dirs::cache_dir().ok_or_else(|| format_err!("Cannot compute user's cache dir"))?;
+    user_cache.push("scriptisto/bin");
+    user_cache.push(script_path_rel);
+    Ok(user_cache)
+}
+
+pub fn write_bytes(cache_path: &Path, rel_path: &Path, data: &[u8]) -> Result<(), Error> {
+    let mut path = cache_path.to_path_buf();
+    path.push(rel_path);
+    debug!("Writing {} bytes to {:?}", data.len(), path);
+    let parent = path
+        .parent()
+        .ok_or_else(|| format_err!("Cannot compute parent path of {:?}", path))?;
+    std::fs::create_dir_all(parent).context(format!(
+        "Cannot create cache directory for script, dir path: {:?}",
+        parent
+    ))?;
+    let mut file = File::create(path).context("Cannot output extra file")?;
+    file.write_all(data).context("Cannot write bytes to file")?;
+    Ok(())
+}
+
+pub fn file_modified(p: &Path) -> Result<std::time::SystemTime, std::io::Error> {
+    let meta = std::fs::metadata(p)?;
+    let modified = meta.modified()?;
+    Ok(modified)
+}
+
+pub fn run_command(
+    current_directory: &Path,
+    mut cmd: Command,
+    stderr_mode: Stdio,
+) -> Result<std::process::Output, Error> {
+    cmd.stdout(Stdio::piped())
+        .stderr(stderr_mode)
+        .current_dir(current_directory);
+
+    debug!("Running command: {:?}", cmd);
+
+    let out = cmd.output().context(format!("Cannot run: {:?}", cmd))?;
+
+    let stderr = String::from_utf8_lossy(&out.stderr);
+    let stdout = String::from_utf8_lossy(&out.stdout);
+    debug!(
+        "Command result: {:?}\nstderr:\n{}\nstdout:\n{}",
+        out.status.code(),
+        stderr,
+        stdout
+    );
+
+    if !out.status.success() {
+        eprintln!("{}", stderr);
+        eprintln!("{}", stdout);
+        let error = match out.status.code() {
+            Some(code) => format_err!("Command {:?} failed. Exit code: {}.", cmd, code,),
+            None => format_err!("Child build process terminated by signal"),
+        };
+        return Err(error);
+    }
+
+    Ok(out)
+}
--- src/main.rs
+++ src/main.rs
@@ -17,87 +17,14 @@ extern crate include_dir;
 
 use failure::{format_err, Error, ResultExt};
 use log::debug;
-use std::fs::File;
-use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::process::{exit, Command, Stdio};
 use std::str::FromStr;
 
 mod cfg;
+mod common;
 mod opt;
 mod templates;
-
-fn build_cache_path(script_path: &Path) -> Result<PathBuf, Error> {
-    let script_path = script_path
-        .canonicalize()
-        .context("Cannot build full path from given script path")?;
-    let script_path_rel = script_path
-        .strip_prefix("/")
-        .context(format!("Could not strip '/' prefix from {:?}", script_path))?;
-
-    let mut user_cache =
-        dirs::cache_dir().ok_or_else(|| format_err!("Cannot compute user's cache dir"))?;
-    user_cache.push("scriptisto/bin");
-    user_cache.push(script_path_rel);
-    Ok(user_cache)
-}
-
-fn write_bytes(cache_path: &Path, rel_path: &Path, data: &[u8]) -> Result<(), Error> {
-    let mut path = cache_path.to_path_buf();
-    path.push(rel_path);
-    debug!("Writing {} bytes to {:?}", data.len(), path);
-    let parent = path
-        .parent()
-        .ok_or_else(|| format_err!("Cannot compute parent path of {:?}", path))?;
-    std::fs::create_dir_all(parent).context(format!(
-        "Cannot create cache directory for script, dir path: {:?}",
-        parent
-    ))?;
-    let mut file = File::create(path).context("Cannot output extra file")?;
-    file.write_all(data).context("Cannot write bytes to file")?;
-    Ok(())
-}
-
-fn file_modified(p: &Path) -> Result<std::time::SystemTime, std::io::Error> {
-    let meta = std::fs::metadata(p)?;
-    let modified = meta.modified()?;
-    Ok(modified)
-}
-
-fn run_command(
-    current_directory: &Path,
-    mut cmd: Command,
-    stderr_mode: Stdio,
-) -> Result<std::process::Output, Error> {
-    cmd.stdout(Stdio::piped())
-        .stderr(stderr_mode)
-        .current_dir(current_directory);
-
-    debug!("Running command: {:?}", cmd);
-
-    let out = cmd.output().context(format!("Cannot run: {:?}", cmd))?;
-
-    let stderr = String::from_utf8_lossy(&out.stderr);
-    let stdout = String::from_utf8_lossy(&out.stdout);
-    debug!(
-        "Command result: {:?}\nstderr:\n{}\nstdout:\n{}",
-        out.status.code(),
-        stderr,
-        stdout
-    );
-
-    if !out.status.success() {
-        eprintln!("{}", stderr);
-        eprintln!("{}", stdout);
-        let error = match out.status.code() {
-            Some(code) => format_err!("Command {:?} failed. Exit code: {}.", cmd, code,),
-            None => format_err!("Child build process terminated by signal"),
-        };
-        return Err(error);
-    }
-
-    Ok(out)
-}
 
 fn run_build_command<F>(
     cfg: &cfg::BuildSpec,
@@ -113,7 +40,7 @@ where
         if let Some(build_once_cmd) = &cfg.build_once_cmd {
             let mut cmd = Command::new("/bin/sh");
             cmd.arg("-c").arg(build_once_cmd);
-            run_command(&script_cache_path, cmd, stderr_mode())?;
+            common::run_command(&script_cache_path, cmd, stderr_mode())?;
         }
     }
 
@@ -123,7 +50,7 @@ where
             Some(docker_build) if docker_build.dockerfile.is_some() => {
                 // Write Dockerfile.
                 let tmp_dockerfile_name = "Dockerfile.scriptisto";
-                write_bytes(
+                common::write_bytes(
                     &script_cache_path,
                     &PathBuf::from(&tmp_dockerfile_name),
                     docker_build.dockerfile.clone().unwrap().as_bytes(),
@@ -162,7 +89,7 @@ where
                     .arg(&tmp_dockerfile_name)
                     .arg(".");
 
-                run_command(&script_cache_path, build_im_cmd, stderr_mode())?;
+                common::run_command(&script_cache_path, build_im_cmd, stderr_mode())?;
 
                 // Build binary in Docker.
                 let mut cmd = Command::new("docker");
@@ -182,19 +109,19 @@ where
                     .arg("-c")
                     .arg(build_cmd);
 
-                run_command(&script_cache_path, cmd, stderr_mode())?;
+                common::run_command(&script_cache_path, cmd, stderr_mode())?;
             }
 
             _ => {
                 let mut cmd = Command::new("/bin/sh");
                 cmd.arg("-c").arg(build_cmd);
 
-                run_command(&script_cache_path, cmd, stderr_mode())?;
+                common::run_command(&script_cache_path, cmd, stderr_mode())?;
             }
         }
     }
 
-    write_bytes(
+    common::write_bytes(
         &script_cache_path,
         &PathBuf::from("scriptisto.metadata"),
         String::new().as_bytes(),
@@ -212,7 +139,7 @@ fn build(
     let script_path = Path::new(script_path);
 
     let script_body = std::fs::read(&script_path).context("Cannot read script file")?;
-    let script_cache_path = build_cache_path(script_path).context(format!(
+    let script_cache_path = common::build_cache_path(script_path).context(format!(
         "Cannot build cache path for script: {:?}",
         script_path
     ))?;
@@ -222,8 +149,8 @@ fn build(
 
     let mut metadata_path = script_cache_path.clone();
     metadata_path.push("scriptisto.metadata");
-    let metadata_modified = file_modified(&metadata_path).ok();
-    let script_modified = file_modified(&script_path).ok();
+    let metadata_modified = common::file_modified(&metadata_path).ok();
+    let script_modified = common::file_modified(&script_path).ok();
 
     let first_run = metadata_modified.is_none();
     let skip_rebuild = metadata_modified > script_modified && build_mode == opt::BuildMode::Default;
@@ -232,7 +159,7 @@ fn build(
         debug!("Already compiled, skipping compilation");
     } else {
         for file in cfg.files.iter() {
-            write_bytes(
+            common::write_bytes(
                 &script_cache_path,
                 &PathBuf::from(&file.path),
                 &file.content.as_bytes(),
