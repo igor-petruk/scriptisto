bb5f5612b5bdea7780ef47fc82f638ebdb47382a
Andreas Sommer <andreas.sommer87@googlemail.com>
2020-11-01 18:51:03 +0100

    Add Dart multi-file template which checks the whole project directory for changes
    This also adds environment variables `SCRIPTISTO_CACHE_DIR` and `SCRIPTISTO_SOURCE_DIR` to the build command. An additional configuration `build_in_script_dir = true` can be used to set the script directory as working directory when running the build.

---
+++ data/templates/dart_multifile_project/functions.dart
@@ -0,0 +1,7 @@
+// This file will also be checked for changes by scriptisto
+
+import 'dart:io';
+
+String getWorkingDirectory() {
+  return Directory.current.path;
+}
---
+++ data/templates/dart_multifile_project/main.dart
@@ -0,0 +1,6 @@
+import 'functions.dart';
+
+void main(List<String> arguments) {
+  print(
+      'Hello Dart, arguments=$arguments getWorkingDirectory()=${getWorkingDirectory()}');
+}
