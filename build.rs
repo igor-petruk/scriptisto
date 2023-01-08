a8787a0e0f76544666b49ae9e560d41f0bd7623b
Igor Petruk <ipetruk@google.com>
2023-01-08 15:05:23 +0000

    Generate man pages recursively

--- build.rs
+++ build.rs
@@ -3,19 +3,36 @@
 mod opt;
 
 use clap::CommandFactory;
+use std::path::Path;
 
-fn main() -> std::io::Result<()> {
+fn generate_command(man_path: &Path, cmd: &clap::Command) -> std::io::Result<()> {
+    let man = clap_mangen::Man::new(cmd.clone());
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+
+    std::fs::write(man_path.join(format!("{}.1", cmd.get_name())), buffer)?;
+
+    for sub in cmd.get_subcommands() {
+        let sub_name: String = format!("{}-{}", cmd.get_name(), sub.get_name()).to_string();
+        generate_command(man_path, &sub.clone().name(sub_name))?;
+    }
+
+    Ok(())
+}
+
+fn generate_man_page() -> std::io::Result<()> {
     let current_dir = std::env::current_dir()?;
     let man_path = current_dir.join("./target/man");
     std::fs::create_dir_all(&man_path)?;
 
     let cmd = opt::Opt::command();
+    generate_command(&man_path, &cmd)?;
 
-    let man = clap_mangen::Man::new(cmd);
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
+    Ok(())
+}
 
-    std::fs::write(man_path.join("scriptisto.1"), buffer)?;
+fn main() -> std::io::Result<()> {
+    generate_man_page()?;
 
     Ok(())
 }
